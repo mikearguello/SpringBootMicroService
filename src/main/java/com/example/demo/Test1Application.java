a213acc842b7432ddf498c80340f91f0628c28c5
root <root@Ryzen1>
2017-09-04 23:39:55 -0500

    current

--- src/main/java/com/example/demo/Test1Application.java
+++ src/main/java/com/example/demo/Test1Application.java
@@ -2,11 +2,32 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.PathSelectors;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+    MainController.class
+})
 public class Test1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Test1Application.class, args);
-	}
+    public static void main(String[] args) {
+            SpringApplication.run(Test1Application.class, args);
+    }
+        
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
